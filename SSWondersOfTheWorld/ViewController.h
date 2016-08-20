1ff2e52f0f1bb58d9f5649a599417a4a72c17274
Sandesh Samgir <samgirsan@outlook.com>
2016-08-21 03:29:25 +1000

    add files via upload

---
+++ SSWondersOfTheWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSWondersOfTheWorld
+//
+//  Created by Amit Bobade on 20/08/16.
+//  Copyright © 2016 Sandesh Samgir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SSWondersOfTheWorld/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SSWondersOfTheWorld
+//
+//  Created by Amit Bobade on 20/08/16.
+//  Copyright © 2016 Sandesh Samgir. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
